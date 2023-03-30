87add7aeccefa556f92b716bcd4de987133f7c12
ryan-bachir <ryan.s.bachir@gmail.com>
2023-03-30 12:05:54 -0400

    created generate attributes and clan, but no code

--- vampire_system.py
+++ vampire_system.py
@@ -1,5 +1,14 @@
 import ttrpg_system
+import random
 
 
 class VampireSystem(ttrpg_system.TTRPGSystem):
     "Represents the tabletop system of vampire the masquerade"
+
+    def generate_clan():
+        # decide bane as well in here prob
+    def generate_attributes():
+        # just an idea, very incomplete
+        attributes = [4, 3, 3, 3, 2, 2, 2, 2, 1]
+        random.shuffle(attributes)
+
